68b123953ebb05f9a791887c3f2298cf405cf29f
Jrar10 <joel.alvarez@edu.cobianchi.it>
2023-03-23 11:41:14 +0100

    add carica matrice

--- src/Model/Matrice.java
+++ src/Model/Matrice.java
@@ -1,11 +1,15 @@
 package model;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+import javax.swing.*;
 
-public class Matrice {
+public class Matrice implements ActionListener {
     private char[][] matrice;
     public Matrice() {
         this.matrice = new char[5][5];
@@ -36,5 +40,46 @@ public class Matrice {
         }
     }
     public void caricaFileToArray() {
+        File fileDizi = new File("src/model/Dizionario.txt"); // Replace "file.txt" with the path to your text file
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(fileDizi);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> words = new ArrayList<>();
+
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            words.add(word);
+        }
+
+        scanner.close();
+
+        // Print the contents of the array to check that it worked
+        /*for (String word : words) {
+            System.out.println(word);
+        }*/
+    }
+
+    //ricercaParolaInMatriceButton
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton) e.getSource();
+        String buttonName = button.getText();
+        System.out.println("Hai cliccato il bottone: " + buttonName);
+    }
+    public void ricercaParolaInMatriceTastiera() {
+        boolean parolaTrov = false;
+        do {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Inserisci una stringa: ");
+            String inputString = scanner.nextLine();
+
+            System.out.println("Hai inserito: " + inputString);
+
+            scanner.close();
+        } while (parolaTrov != true);
     }
 }
--- src/pp/Main.java
+++ src/pp/Main.java
@@ -8,18 +8,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        //GuiMenu menu = new GuiMenu("Il Paroliere - Menu");
-        //menu.setVisible(true);
+        GuiMenu menu = new GuiMenu();
 
-        GuiMenu2 menu = new GuiMenu2();
+        /*GuiMenu2 menu = new GuiMenu2();
         menu.setContentPane(menu.getPanel());
         menu.setTitle("Il Paroliere - Menu");
         menu.setSize(400,200);
-        menu.setVisible(true);
+        menu.setVisible(true);*/
 
         Matrice griglia = new Matrice();
         griglia.caricaMatrice();
         griglia.visualMatrice();
-        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        griglia.caricaFileToArray();
+        //menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
