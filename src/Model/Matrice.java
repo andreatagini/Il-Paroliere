5d51c95015494d39ad0fbbf32b0f49a26efb5c72
Jrar10 <joel.alvarez@edu.cobianchi.it>
2023-04-04 11:46:12 +0200

    add carica matrice

--- src/Model/Matrice.java
+++ src/Model/Matrice.java
@@ -10,11 +10,23 @@ import java.util.Scanner;
 import javax.swing.*;
 
 public class Matrice implements ActionListener {
+
+    //attributi
     private char[][] matrice;
+    private ArrayList<String> words = new ArrayList<>();
+
+    //costruttore
     public Matrice() {
         this.matrice = new char[5][5];
     }
 
+    //getter and setter
+    public ArrayList<String> getWords() {
+        return words;
+    }
+    public void setWords(ArrayList<String> words) {
+        this.words = words;
+    }
     public char[][] getMatrice() {
         return matrice;
     }
@@ -48,8 +60,6 @@ public class Matrice implements ActionListener {
             e.printStackTrace();
         }
 
-        ArrayList<String> words = new ArrayList<>();
-
         while (scanner.hasNext()) {
             String word = scanner.next();
             words.add(word);
@@ -69,17 +79,84 @@ public class Matrice implements ActionListener {
         String buttonName = button.getText();
         System.out.println("Hai cliccato il bottone: " + buttonName);
     }
-    public void ricercaParolaInMatriceTastiera() {
-        boolean parolaTrov = false;
-        do {
+
+    // Custom listener class to handle input events
+    public static class InputListener implements Runnable {
+        private char input;
+
+        public void run() {
             Scanner scanner = new Scanner(System.in);
 
+            // Continuously read input from the scanner and signal an event when input is received
+            while (true) {
+                if (scanner.hasNext()) {
+                    synchronized (this) {
+                        input = scanner.next().charAt(0);
+                        notify();
+                    }
+                }
+            }
+        }
+
+        public char getInput() {
+            return input;
+        }
+    }
+    public void ricercaParolaInMatriceTastiera() {
+        Scanner scanner = new Scanner(System.in);
+        InputListener listener = new InputListener();
+
+        // Create a new thread for the InputListener and start it
+        Thread listenerThread = new Thread(listener);
+        listenerThread.start();
+
+        // Register the listener with the scanner
+        scanner.useDelimiter("");
+
+        // Start the event loop
+        while (true) {
+            // Wait for the listener to signal an event
+            System.out.print("Inserisci una stringa: ");
+            synchronized (listener) {
+                try {
+                    listener.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            // Get the character from the listener and handle the event
+            char c = listener.getInput();
+            if (c == 'a') {
+                System.out.println("A key pressed");
+            } else if (c == 'b') {
+                System.out.println("B key pressed");
+            } else if (c == 'q') {
+                System.out.println("Exiting program...");
+                break;
+            }
+        }
+        /*boolean parolaTrov = false;
+        do {
+            Scanner scanner = new Scanner(System.in);
             System.out.print("Inserisci una stringa: ");
             String inputString = scanner.nextLine();
-
             System.out.println("Hai inserito: " + inputString);
 
+            for (int i = 0; i < matrice.length; i++) {
+                for (int j = 0; j < matrice.length; j++) {
+
+                }
+                String fruit = words.get(i);
+                if (fruit.equals(inputString)) {
+                    parolaTrov = true;
+                    System.out.println("Parola Trovata");
+                    break;
+                }
+            }
+            if (parolaTrov == false) {
+                System.out.println("Parola non Trovata");
+            }
             scanner.close();
-        } while (parolaTrov != true);
+        } while (parolaTrov != true);*/
     }
 }
