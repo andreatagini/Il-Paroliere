883360c3ca04043822615df1e9b557a9c2d055ab
Jrar10 <joel.alvarez@edu.cobianchi.it>
2023-05-02 11:56:19 +0200

    add carica matrice

--- src/pp/Main.java
+++ src/pp/Main.java
@@ -6,6 +6,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        GuiMenu2 menu = new GuiMenu2();
+        menu.setContentPane(menu.getPanel());
+        menu.setTitle("Il Paroliere - Menu");
+        menu.setSize(400,200);
+        menu.setVisible(true);
+
         Matrice griglia = new Matrice();
         griglia.caricaMatrice();
         griglia.visualMatrice();
