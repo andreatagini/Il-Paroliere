2837b519dc5d07cfe72170452a5f2e0fb569b01c
Jrar10 <joel.alvarez@edu.cobianchi.it>
2023-05-18 10:47:25 +0200

    dai

--- src/Model/Matrice.java
+++ src/Model/Matrice.java
@@ -59,47 +59,6 @@ public class Matrice implements ActionListener {
                 this.matrice[i][j] = alphabet.charAt(r.nextInt(alphabet.length()));
             }
         }
-        for (int j = 0; j < 7; j++) {
-            //prendo string casuale dall'array parole comuni
-            Random r1 = new Random();
-            int nCas1 = r1.nextInt(getParoleComuni().size());
-            String comune = getParoleComuni().get(nCas1);
-            //numeri random per la posizione della matrice
-            Random rx = new Random();
-            int nCasX = rx.nextInt(10);//numero compreso tra 0 e 9
-            Random ry = new Random();
-            int nCasY = ry.nextInt(10);
-            //numero random per la direzione in cui verra inserita
-            int nCas2 = isDentroRange(nCasX, nCasY, comune);
-            //inserimento di parole comuni nella matrice
-            matrice[nCasY][nCasX] = comune.charAt(0);
-            for (int i = 1; i < comune.length(); i++) {
-                if (nCas2 == 1) {
-                    matrice[nCasY][nCasX+i] = comune.charAt(i);
-                }
-                if (nCas2 == 2) {
-                    matrice[nCasY+i][nCasX+i] = comune.charAt(i);
-                }
-                if (nCas2 == 3) {
-                    matrice[nCasY+i][nCasX] = comune.charAt(i);
-                }
-                if (nCas2 == 4) {
-                    matrice[nCasY+i][nCasX-i] = comune.charAt(i);
-                }
-                if (nCas2 == 5) {
-                    matrice[nCasY][nCasX-i] = comune.charAt(i);
-                }
-                if (nCas2 == 6) {
-                    matrice[nCasY-i][nCasX-i] = comune.charAt(i);
-                }
-                if (nCas2 == 7) {
-                    matrice[nCasY-i][nCasX] = comune.charAt(i);
-                }
-                if (nCas2 == 8) {
-                    matrice[nCasY-i][nCasX+i] = comune.charAt(i);
-                }
-            }
-        }
     }
     public int isDentroRange(int nCasX, int nCasY, String comune) {
         boolean inRange = false;
@@ -108,6 +67,7 @@ public class Matrice implements ActionListener {
             //numero random per la direzione in cui verra inserita
             Random r2 = new Random();
             nCas2 = r2.nextInt(8) + 1;//numero compreso tra 1 e 8
+            System.out.print(nCas2);
             if (nCas2 == 1) {
                 if (nCasX + comune.length() < matrice[0].length) {
                     inRange = true;
