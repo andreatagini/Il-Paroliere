aec17d4fc06891fa49acee13c677378983d9200b
Jrar10 <joel.alvarez@edu.cobianchi.it>
2023-05-21 19:02:41 +0200

    aggiunta di parole comuni nella matrice

--- src/pp/Main.java
+++ src/pp/Main.java
@@ -3,25 +3,14 @@ package pp;
 import Model.Matrice;
 
 import javax.swing.*;
-import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args){
         GuiMenu2 menu = new GuiMenu2();
         menu.setContentPane(menu.getPanel());
         menu.setTitle("Il Paroliere - Menu");
         menu.setSize(400,200);
         menu.setVisible(true);
-
-        /*Matrice griglia = new Matrice();
-        griglia.caricaMatrice();
-        griglia.visualMatrice();
-        boolean find = griglia.ricercaParolaInMatriceTastiera("ciao");
-        if (find == true) {
-            System.out.println("Parola trovata");
-        } else {
-            System.out.println("Parola non trovata");
-        }*/
 
         menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
